872260bf4eefd3e1c7420201e1389053ff7752b9
Gaurav Dhingra <gaurav.dhingra@usbank.com>
2023-12-04 09:58:39 +0530

    Domain Module Test Case

---
+++ domain/src/test/java/com/ps/domain/usecase/GetActorsUseCaseTest.kt
@@ -0,0 +1,43 @@
+package com.ps.domain.usecase
+
+import com.ps.common.utils.Resource
+import com.ps.domain.model.ActorsModel
+import com.ps.domain.repository.ActorsRepository
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import java.io.IOException
+
+class GetActorsUseCaseTest {
+    private val mockActorsRepository = mockk<ActorsRepository>()
+
+    // UseCase
+    private lateinit var getActorsUseCase: GetActorsUseCase
+
+    // Test data
+    private val mockActorsModel = mockk<ActorsModel>()
+    @Before
+    fun setUp() {
+        getActorsUseCase =
+            GetActorsUseCase(mockActorsRepository, Dispatchers.Unconfined)
+    }
+
+    @Test
+    fun `invoke returns Resource Success`() = runBlocking {
+        coEvery { mockActorsRepository.getActors() } returns Resource.Success(mockActorsModel)
+        val result = getActorsUseCase.invoke()
+        Assert.assertTrue(result is Resource.Success)
+    }
+
+    @Test
+    fun `invoke returns Resource Failure`() = runBlocking {
+        val ioException = IOException("An error occurred")
+        coEvery {  mockActorsRepository.getActors() } returns Resource.Failure(ioException)
+        val result = getActorsUseCase.invoke()
+        Assert.assertTrue(result is Resource.Failure)
+    }
+}
